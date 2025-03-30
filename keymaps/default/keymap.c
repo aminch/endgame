c18d4cfcdaf21057aece7616c39bbf7d450bc084
Adam Minchinton <adam.minchinton@gmail.com>
2025-03-30 13:07:35 +0200

    Add the symbol layer

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -1,4 +1,5 @@
 #include QMK_KEYBOARD_H
+#include "keymap_us_international.h"
 
 enum layer_names {
     _QWERTY,
@@ -32,18 +33,21 @@ enum layer_names {
 enum combo_events {
   CAPSWORD_COMBO,
   CAPSLOCK_COMBO,
-  QUOT_COMBO
+  QUOT_COMBO,
+  BOOT_COMBO
 };
 
 #ifdef COMBO_ENABLE
 const uint16_t PROGMEM capsword_combo[] = {KC_F, KC_J, COMBO_END};
 const uint16_t PROGMEM capslock_combo[] = {KC_W, KC_O, COMBO_END};
 const uint16_t PROGMEM quot_combo[] = {KC_U, KC_I, COMBO_END};
+const uint16_t PROGMEM boot_combo[] = {KC_F1, KC_F10, COMBO_END};
 
 combo_t key_combos[] = {
     [CAPSWORD_COMBO] = COMBO(capsword_combo, CW_TOGG),
     [CAPSLOCK_COMBO] = COMBO(capslock_combo, KC_CAPS),
-    [QUOT_COMBO] = COMBO(quot_combo, KC_QUOT)
+    [QUOT_COMBO] = COMBO(quot_combo, KC_QUOT),
+    [BOOT_COMBO] = COMBO(boot_combo, QK_BOOT)
 };
 #endif // COMBO_ENABLE
 
@@ -52,34 +56,45 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
     CTL_A,    ALT_S,    GUI_D,    SFT_F,    RALT_G,             RALT_H,   SFT_J,    GUI_K,    KC_L,     CTL_SEMI,
     KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,
-    KC_LSFT,                      NUM_ESC,  NAV_SPC,            SYM_BSPC, FUN_ENT,                      KC_RSFT
+    KC_LCTL,                      NUM_ESC,  NAV_SPC,            SYM_BSPC, FUN_ENT,                      KC_BSPC
   ),
   
   [_NUMBER] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            KC_PLUS,  KC_7,  KC_8,  KC_9,  XXXXXXX,
-    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  XXXXXXX,            KC_MINS,  KC_4,  KC_5,  KC_6,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_1,  KC_2,  KC_3,  XXXXXXX,
-    XXXXXXX,                      XXXXXXX,  XXXXXXX,            KC_E,     KC_0,                XXXXXXX
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            KC_PPLS,  KC_7,  KC_8,  KC_9,  KC_PSLS,
+    KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  XXXXXXX,            KC_PMNS,  KC_4,  KC_5,  KC_6,  KC_PAST,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_1,  KC_2,  KC_3,  KC_PDOT,
+    KC_LSFT,                      XXXXXXX,  XXXXXXX,            KC_E,     KC_0,                KC_BSPC
   ),
   
   [_NAV] = LAYOUT(
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_TAB,             KC_INS,  KC_HOME,  XXXXXXX, KC_PGUP,  XXXXXXX,
     KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  KC_CAPS,            KC_DEL,  KC_END,   KC_UP,   KC_PGDN,  XXXXXXX,
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_LEFT, KC_DOWN, KC_RGHT,  XXXXXXX,
-    XXXXXXX,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                    XXXXXXX
+    KC_LCTL,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                    KC_BSPC
   ),
   
   [_SYMBOL] = LAYOUT(
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                      XXXXXXX
+    KC_GRV,  KC_TILD,  KC_PIPE,  XXXXXXX,  KC_LBRC,            KC_RBRC,  KC_AMPR,  KC_ASTR,  KC_QUOT,  KC_DQT,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LPRN,            KC_RPRN,  KC_DLR,   KC_PERC,  KC_CIRC,  XXXXXXX,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_LABK,            KC_RABK,  KC_EXLM,  KC_AT,    KC_HASH,  KC_UNDS,
+    KC_LSFT,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                      KC_BSPC
   ),
   
   [_FUNCTION] = LAYOUT(
-    QK_BOOT,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_F7,  KC_F8,  KC_F9,  KC_F12,
+    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_F7,  KC_F8,  KC_F9,  KC_F12,
     KC_LCTL,  KC_LALT,  KC_LGUI,  KC_LSFT,  XXXXXXX,            XXXXXXX,  KC_F4,  KC_F5,  KC_F6,  KC_F11,
     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,            XXXXXXX,  KC_F1,  KC_F2,  KC_F3,  KC_F10,
-    XXXXXXX,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                XXXXXXX
+    KC_LCTL,                      XXXXXXX,  XXXXXXX,            XXXXXXX,  XXXXXXX,                KC_BSPC
   ),
 };
+
+bool combo_should_trigger(uint16_t combo_index, combo_t *combo, uint16_t keycode, keyrecord_t *record) {
+  switch (combo_index) {
+      case BOOT_COMBO:
+          if (!layer_state_is(_FUNCTION)) {
+              return false;
+          }
+  }
+
+  return true;
+}
